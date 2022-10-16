e7a7a6bf8789430298a8b1b9fda2b9138eb63063
Vaidehi-Kokare <vaidehikokare@gmail.com>
2022-10-16 19:43:09 +0530

    Day 5 Practice 02

---
+++ Day5/Practice_Problems/Unit_conversion.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+echo "1 foot is 12 Inches";
+a=$((42*12));
+echo "Value of 42 Feet is:" $a "Inches"
+echo "Length of plot is 60 ft ";
+echo "Breadth of plot is 40 ft";
+Length=$60;
+Breadth=$40;
+echo "Length of plot in meters is:" $Length*0.3048
+echo "Breadth of plot in meters is" $breadth*0.3048
