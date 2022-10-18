fabe8d3b0eabdc6d986b4a9ccd0232d131875fd7
Vaidehi-Kokare <vaidehikokare@gmail.com>
2022-10-18 16:09:00 +0530

    Day 5 Practice 02

---
+++ Day5/Practice_Problems/Place_value_case.sh
@@ -0,0 +1,19 @@
+read -p "Enter 1 or multiple of 10 : " num;
+case $num in
+  1)
+      echo "Unit";
+    ;;
+    10)
+    echo "Ten";
+      ;;
+      100)
+      echo "Hundred";
+        ;;
+        1000)
+        echo "Thousand";
+          ;;
+          10000)
+          echo "Ten Thousand";
+            ;;
+          
+esac
